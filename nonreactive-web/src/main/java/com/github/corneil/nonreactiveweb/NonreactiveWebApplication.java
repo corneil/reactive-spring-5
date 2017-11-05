f1b65f2adccc370391988b96753464af83e09901
corneil <8720cdp>
2017-11-05 10:58:18 +0200

    initial add

---
+++ nonreactive-web/src/main/java/com/github/corneil/nonreactiveweb/NonreactiveWebApplication.java
@@ -0,0 +1,12 @@
+package com.github.corneil.nonreactiveweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonreactiveWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonreactiveWebApplication.class, args);
+	}
+}
---
+++ nonreactive-web/src/test/java/com/github/corneil/nonreactiveweb/NonreactiveWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.corneil.nonreactiveweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NonreactiveWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
